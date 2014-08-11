218a9f68d66c2aed8dcd2b1294b3898db070b731
Alejandro Such Berenguer <alejandro.such@gmail.com>
2014-08-11 18:01:52 +0200

    Like, comment, act as comment or message

--- components/directives/message/directive/Message.js
+++ components/directives/message/directive/Message.js
@@ -1,12 +1,33 @@
 angular
     .module('asvook')
-    .directive('message', function(){
+    .directive('message', function () {
         return {
             restrict: 'E',
             require: '^ngModel',
             scope: {
-                message : '=ngModel'
+                message: '=ngModel',
+                isComment : '='
             },
-            templateUrl: 'components/directives/message/template/message.html'
+            templateUrl: 'components/directives/message/template/message.html',
+            controller: function ($scope, WallServices, $location, appRoutes) {
+                $scope.alreadyLikes = false;
+                $scope.like = function () {
+                    var increment = 1;
+                    if ($scope.alreadyLikes) {
+                        increment = -1;
+                    }
+
+                    WallServices
+                        .addLike($scope.message, increment)
+                        .then(function () {
+                            $scope.alreadyLikes = !$scope.alreadyLikes;
+                            $scope.message.counters.likesCount += increment;
+                        });
+                };
+
+                $scope.comment = function(){
+                    $location.path(appRoutes.comments + $scope.message.id);
+                };
+            }
         };
     });
