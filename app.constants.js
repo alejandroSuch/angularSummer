576e83f29975f99fc23fa11a57f6321cc14fef40
Alejandro Such Berenguer <alejandro.such@gmail.com>
2014-08-11 18:02:32 +0200

    app routes

--- app.constants.js
+++ app.constants.js
@@ -1,3 +1,9 @@
 angular
     .module('asvook')
-    .constant('servicesUrl', 'http://localhost:9000/services/');
+    .constant('servicesUrl', 'http://localhost:9000/services/')
+    .factory('appRoutes', function () {
+        return {
+            wall: '/wall/',
+            comments: '/comments/'
+        }
+    });
