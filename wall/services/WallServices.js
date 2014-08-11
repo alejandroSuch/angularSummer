3cb636e689b50799855e0ccf612ce542b207d89b
Alejandro Such Berenguer <alejandro.such@gmail.com>
2014-08-11 18:02:08 +0200

    addLike, getMessageWithComments

--- wall/services/WallServices.js
+++ wall/services/WallServices.js
@@ -1,20 +1,47 @@
 angular
     .module('asvook')
-    .factory('WallServices', function($http, servicesUrl){
+    .factory('WallServices', function ($http, servicesUrl) {
         var instance = {
-            getWall : function(){
+            getWall: function () {
                 return $http
                     .get(servicesUrl + 'wall')
-                    .then(function(response){
+                    .then(function (response) {
                         return response.data;
                     })
-                    .catch(function(){
+                    .catch(function () {
                         console.log('error!!', arguments);
                     })
-                    .finally(function(){
+                    .finally(function () {
 
                     });
+            },
 
+            addLike: function (message, increment) {
+                return $http
+                    .post(servicesUrl + 'likes', { status: message.id, increment: increment})
+                    .then(function (response) {
+                        return response.data;
+                    })
+                    .catch(function () {
+                        console.log('error!!', arguments);
+                    })
+                    .finally(function () {
+
+                    });
+            },
+
+            getMessageWithComments : function(id) {
+                return $http
+                    .get(servicesUrl + 'comments/' + id)
+                    .then(function (response) {
+                        return response.data;
+                    })
+                    .catch(function () {
+                        console.log('error!!', arguments);
+                    })
+                    .finally(function () {
+
+                    });
             }
         };
 
