52dc7af71f0e92e221d1942783d0aaa97f542049
Alejandro Such Berenguer <alejandro.such@gmail.com>
2014-08-12 17:52:27 +0200

    Directives used on wall, and put on comments

--- comments/controllers/CommentsCtrl.js
+++ comments/controllers/CommentsCtrl.js
@@ -1,6 +1,8 @@
 angular
     .module('asvook')
     .controller('CommentsCtrl', function ($scope, $location, $routeParams, appRoutes, WallServices) {
+        $scope.newMessage = WallServices.createPost();
+
         if (!/^\d.*$/.test($routeParams.id)) {
             $location.path(appRoutes.wall);
             return;
--- wall/controllers/WallCtrl.js
+++ wall/controllers/WallCtrl.js
@@ -1,8 +1,22 @@
 angular
     .module('asvook')
-    .controller('WallCtrl', function($scope, WallServices){
-        console.log('WallCtrl')
-        WallServices.getWall().then(function(messages){
+    .controller('WallCtrl', function ($scope, WallServices) {
+        var createNewMessage = function () {
+            $scope.newMessage = WallServices.createPost();
+        };
+
+        WallServices.getWall().then(function (messages) {
             $scope.messages = messages;
         });
+
+        $scope.sendMessage = function () {
+            WallServices
+                .sendPost($scope.newMessage)
+                .then(function (message) {
+                    $scope.messages.unshift(message);
+                    createNewMessage();
+                });
+        };
+
+        createNewMessage();
     });
